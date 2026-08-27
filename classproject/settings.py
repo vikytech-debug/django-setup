ff04a3be74579f718f1298e7e581a06195202a02
vikytech-debug <ogunlumadesegun22@gmail.com>
2026-08-27 13:10:09 -0600

    Add whitenoise to requirements and update settings

--- classproject/settings.py
+++ classproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-24=9jq&=e2qn5bu)m#l)xgelew$44i^1k)mb1n#x!*x@^p1znw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
