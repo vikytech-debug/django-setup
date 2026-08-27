cb4477c3492e104954e67cf56f572955d933ca55
vikytech-debug <ogunlumadesegun22@gmail.com>
2026-08-27 12:39:06 -0600

    Initial commit

--- classproject/settings.py
+++ classproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-24=9jq&=e2qn5bu)m#l)xgelew$44i^1k)mb1n#x!*x@^p1znw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -119,3 +120,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR / "static"),)
+STATIC_ROOT= os.path.join(BASE_DIR / "staticfiles")
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
